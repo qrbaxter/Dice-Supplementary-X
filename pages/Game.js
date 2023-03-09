ff43e76faa71320c9dff7c97812eb813e022b49b
qrbaxter <qrbaxter90@gmail.com>
2023-03-09 14:27:55 +1100

    commit
    commit

--- pages/Game.js
+++ pages/Game.js
@@ -4,7 +4,106 @@ import Die from '../Die';
 import { nanoid } from 'nanoid';
 
 function Game() {
+    
+    
+    //Bonus Function1 //
+        function threePairs(dice) {
+  const encounteredDice = {};
+  dice.forEach(die => {
+    if (encounteredDice[die]) {
+      encounteredDice[die]++;
+    } else {
+      encounteredDice[die] = 1;
+    }
+  });
 
+  let numPairs = 0;
+  for (const die in encounteredDice) {
+    if (encounteredDice[die] === 2) {
+      numPairs++;
+    }
+  }
+
+  return numPairs === 3;
+}
+
+function bigMultiplier(dice) {
+  const encounteredDice = {};
+  dice.forEach((die) => {
+    if (encounteredDice[die]) {
+      encounteredDice[die]++;
+    } else {
+      encounteredDice[die] = 1;
+    }
+  });
+
+  let currentScoreMultiplier = 1;
+  let currentScoreToAdd = 0;
+
+  for (const die in encounteredDice) {
+    const count = encounteredDice[die];
+    if (count === 3) {
+      if (die === '1') {
+        currentScoreToAdd += 1000;
+      } else {
+        currentScoreToAdd += parseInt(die, 10) * 100;
+      }
+    } else if (count === 4) {
+      currentScoreMultiplier = 2;
+      currentScoreToAdd += parseInt(die, 10) * 100 * currentScoreMultiplier;
+    } else if (count === 5) {
+      currentScoreMultiplier *= 2;
+      currentScoreToAdd += parseInt(die, 10) * 100 * currentScoreMultiplier;
+    } else if (count === 6) {
+      currentScoreMultiplier *= 2;
+      currentScoreToAdd += parseInt(die, 10) * 100 * currentScoreMultiplier;
+    }
+  }
+
+  return currentScoreToAdd;
+}
+//Bonus Function 2//
+function straightFlush(dice) {
+  const encounteredDice = {};
+  dice.forEach(die => {
+    if (encounteredDice[die]) {
+      encounteredDice[die]++;
+    } else {
+      encounteredDice[die] = 1;
+    }
+  })
+  for (let i = 1; i <= 6; i++) {
+    if (!encounteredDice[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//Bonus Function 3//
+function isTriplet(heldDice) {
+  const encounteredDice = {};
+  heldDice.forEach(die => {
+    if (encounteredDice[die.value]) {
+      encounteredDice[die.value]++;
+    } else {
+      encounteredDice[die.value] = 1;
+    }
+  });
+
+  for (const value in encounteredDice) {
+    if (encounteredDice[value] >= 3) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+//BONUS FUNCTION 4
+
+//
   const [dice, setDice] = useState([
     { id: nanoid(), value: 0, isHeld: false },
     { id: nanoid(), value: 0, isHeld: false },
@@ -13,6 +112,9 @@ function Game() {
     { id: nanoid(), value: 0, isHeld: false },
     { id: nanoid(), value: 0, isHeld: false },
   ]);
+  
+  const diceValues = dice.map((die) => die.value);
+
 
   const [bankedScore, setBankedScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
@@ -25,6 +127,10 @@ function Game() {
   const [endTurnBtnDisabled, setEndTurnBtnDisabled] = useState(true)
   const [bustCount, setBustCount]=useState(0)
   const [endGame, setEndGame]=useState(false)
+  const [message, setMessage] = useState('')
+  const [threeBigPairs, setThreeBigPairs] = useState(false)
+  const [theStraightFlush, setTheStraightFlush] = useState(false)
+  const [theTriplet, setTheTriplet] = useState(false)
 
 
   function holdDice(id) {
@@ -34,53 +140,134 @@ function Game() {
         die
     }))
   }
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+function bankPoints() {
+  const heldDiceValues = dice.filter(die => die.isHeld).map(die => die.value);
     
-  const bankPoints = () => {
-  if (!busted) {
-    setRollBtnDisabled(false);
-  } else {
-    setRollBtnDisabled(false);
-    setRollBtnDisabled(true);
-    setBankerinoBlock(true)
+    if (isTriplet(heldDiceValues)){
+           const counts = {};
+    heldDiceValues.forEach(value => {
+    counts[value] = (counts[value] || 0) + 1;
+    })
+    for (const value in counts) {
+    let scoreToAdd = 0;
+    let messageToAdd = '';
+    const count = counts[value]
+   if (count >= 3) {
+       setBusted(false)
+  if (value === 1) {
+    scoreToAdd += value * 1000 * (2 ** (count - 3));
+  } else if (value === 2 || 3 || 4 || 5 || 6){
+    scoreToAdd += value * 100 * (2 ** (count - 3));
+    
+  }
+  messageToAdd = `Multiplier x${count}!`;
+} 
+
+
+setMessage(messageToAdd);
+setTheTriplet(true);
+setCurrentScore(currentScore + scoreToAdd)
+    
+    }
+
+
+  
+    }
+    
+
+    
+   
+   
+   
+   
+   ///////////////////////////////////////////
+   
+   //////////////////////////////////////////// 
+  if (straightFlush(heldDiceValues)) {
+    setDice(oldDice => oldDice.map(die => (
+      heldDiceValues.includes(die.value) ? {...die, value: 0} : die
+    )));
+    setTheStraightFlush(true);
+    setCurrentScore(currentScore + 1000);
+    setNewDiceBtnDisabled(false);
+    return;
   }
 
-  let ones = 0;
-  let twos = 0;
-  let threes = 0;
-  let fours = 0;
-  let fives = 0;
-  let sixes = 0;
-  let bonus = 0;
-  let newDice = [...dice];
- 
-  for (let i = 0; i < 6; i++) {
-    if (newDice[i].isHeld && (newDice[i].value === 1 || newDice[i].value === 5)) {
-      if (newDice[i].value === 1) {
-        ones++;
-      } else {
-        fives++;
+
+    const bigScore = bigMultiplier(heldDiceValues);
+if (bigScore > 0) {
+    setDice(oldDice => oldDice.map(die => (
+      heldDiceValues.includes(die.value) ? {...die, value: 0} : die
+    )));
+  setCurrentScore(currentScore + bigScore);
+  setNewDiceBtnDisabled(false);
+  
+  /////////MAKE HELD DICE INVISIBLE
+  return;
+}
+  if (threePairs(heldDiceValues)) {
+    setDice(oldDice => oldDice.map(die => (
+      heldDiceValues.includes(die.value) ? {...die, value: 0} : die
+    )));
+    setThreeBigPairs(true);
+    setCurrentScore(currentScore + 500);
+    setNewDiceBtnDisabled(false);
+    return;
+  } else {
+    if (!busted) {
+      setRollBtnDisabled(false);
+    } else {
+      setRollBtnDisabled(false);
+      setRollBtnDisabled(true);
+      setBankerinoBlock(true);
+    }
+    
+
+    let ones = 0;
+    let fives = 0;
+    let newDice = [...dice];
+
+    for (let i = 0; i < 6; i++) {
+      if (newDice[i].isHeld && (newDice[i].value === 1 || newDice[i].value === 5)) {
+        if (newDice[i].value === 1) {
+          ones++;
+        } else {
+          fives++;
+        }
+        newDice[i].isHeld = false;
+        newDice[i].value = 0;
       }
-      newDice[i].isHeld = false;
-      newDice[i].value = 0
-    } 
-  }
- 
-  setDice(newDice);
-  setCurrentScore(parseInt(currentScore) + ones * 100 + fives * 50);
-  if (ones === 0 && fives === 0) {
-   
+    }
+
+    setDice(newDice);
+    setCurrentScore((currentScore) + ones * 100 + fives * 50);
+    
+    if (ones === 0 && fives === 0) { 
       setBusted(true);
       setCurrentScore(0);
-   
-  } else {
-    setBusted(false);
+    } else {
+      setBusted(false);
+    }
+    
   }
 };
-    
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////////////////////////
+  
+ 
+
+
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const endTurn = () => {
         
         setBankedScore(currentScore+bankedScore)
         setCurrentScore(0)
+        setThreeBigPairs(false)
         newSet()
        
         
@@ -92,6 +279,8 @@ function Game() {
     setEndTurnBtnDisabled(true);
   }
   setRollBtnDisabled(true);
+  setThreeBigPairs(false)
+  setTheTriplet(false)
   let newDice;
   if (currentScore > 0) {
     newDice = dice.map((die) => {
@@ -113,10 +302,9 @@ function Game() {
       setBusted(true);
       setRollBtnDisabled(true);
       setCurrentScore(0);
+      setMessage(<div><h1>YOU BUSTED!</h1></div>)
       return (
-        <div>
-          <h1 className="gameh1">YOU BUSTED!</h1>
-        </div>
+        message
       );
     }, 300);
   } else {
@@ -125,6 +313,9 @@ function Game() {
   setDice(newDice);
 };
 const resetGame = () => {
+    setThreeBigPairs(false)
+    setTheTriplet(false)
+    setTheStraightFlush(false)
     setBankedScore(0)
     setCurrentScore(0)
     localStorage.clear()
@@ -132,8 +323,10 @@ const resetGame = () => {
     newSet()
 }
   const newSet = () => {
-      
-      setRollBtnDisabled(true);
+      setThreeBigPairs(false)
+      setTheStraightFlush(false)
+      setRollBtnDisabled(true)
+      setTheTriplet(false)
     
     setNewDiceBtnDisabled(true);
     setBankerinoBlock(false)
@@ -175,15 +368,18 @@ const resetGame = () => {
             
             <div>
             {busted ?
-            <>
             <h1 className="gameh1">YOU BUSTED!</h1>
-            </> :
-            <>
+            : threeBigPairs ? 
+            <h1 className="gameh1">Three Pairs! +500!</h1>
+           : theStraightFlush ? 
+           <h1 className = "gameh1">Straight Flush, +1000!</h1>
+           :
             <h1 className="gameh1">BANKED: {currentScore}</h1>
-            </>
+            
         }
             </div>
-            
+
+               
             <div>
             <button className="endTurnBtn" onClick={endTurn} disabled={endTurnBtnDisabled}>End Turn</button>
             <button className="resetBtn" onClick={resetGame}>Reset Game</button>
@@ -205,6 +401,8 @@ const resetGame = () => {
             />
           ))}
       </div>
+      
+      
       <div className="buttons1">
   {diceSet && (
     <button className="rollBtn" onClick={rollDice} disabled={rollBtnDisabled}>
@@ -224,5 +422,3 @@ const resetGame = () => {
 }
 
 export default Game;
-
-
